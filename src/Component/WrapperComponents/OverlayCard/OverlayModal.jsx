aaf7851ee8157207ef492d391d20ad83af36f511
Iresh Shivanand Gopanpalli <ireshgopanpalli126@gmail.com>
2024-12-13 12:47:24 +0530

    Update OverlayModal.jsx

--- src/Component/WrapperComponents/OverlayCard/OverlayModal.jsx
+++ src/Component/WrapperComponents/OverlayCard/OverlayModal.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import Modal from '@mui/material/Modal';
-import { Grid, Stack, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { Grid, Stack, Card, CardActionArea, CardContent, CardMedia, Typography, Button } from '@mui/material';
 import MovieCreationRoundedIcon from '@mui/icons-material/MovieCreationRounded';
 import StarsRoundedIcon from '@mui/icons-material/StarsRounded';
 import './OverlayModal.css';
+import CloseIcon from '@mui/icons-material/Close';
 
 const style = {
     position: 'absolute',
     top: '50%',
+    // padding:'1%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: { xs: '90%', sm: '70%', md: '50%', lg: '40%' }, // Responsive width
-    bgcolor: '#0f1014',
+    bgcolor: 'inherit',
     border: '2px solid #16181f',
     boxShadow: 24,
     m: 0,
-    p: 0,
+    // p: 0,
 };
 
 function OverlayModal({ movie, releaseDate, open, handleClose }) {
@@ -27,6 +29,7 @@ function OverlayModal({ movie, releaseDate, open, handleClose }) {
             aria-labelledby="keep-mounted-modal-title"
             aria-describedby="keep-mounted-modal-description"
         >
+
             <Grid container justifyContent="center" alignItems="center" sx={style}>
                 <Grid item xs={12}>
                     <Card className='overlay-card-content' sx={{ maxWidth: '100%', textAlign: 'justify' }}>
@@ -39,13 +42,9 @@ function OverlayModal({ movie, releaseDate, open, handleClose }) {
                                 sx={{ objectFit: 'cover' }}
                             />
                             <CardContent>
-                                <Stack spacing={1} alignItems="center">
+                                <Stack spacing={1} al ignItems="center">
                                     <Typography gutterBottom variant="h6" component="div" color='#fff' textAlign={'center'}>
-                                        {releaseDate} • {movie.original_language.toUpperCase()} • 
-                                        <button className='UAbutton'>U/A{movie.adult ? '18+' : '16+'}</button> • 
-                                        {movie.vote_average} <StarsRoundedIcon /> • 
-                                        {movie.media_type ? movie.media_type.substring(0, 1).toUpperCase() + movie.media_type.substring(1) : 'Series'} 
-                                        <MovieCreationRoundedIcon />
+                                        {movie.name || movie.title} 🎬 • {releaseDate} • {movie.original_language.toUpperCase()} • <button className='UAbutton'>U/A{movie.adult ? '18+' : '16+'}</button> • {movie.vote_average.toFixed(1)} 🌟 • {movie.media_type ? movie.media_type.substring(0, 1).toUpperCase() + movie.media_type.substring(1) : 'Series'} 🍿
                                     </Typography>
                                     <Typography variant="body1" sx={{ color: '#b1c1e6', fontWeight: 'lighter' }}>
                                         {movie.overview}
